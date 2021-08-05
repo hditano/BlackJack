b1021717f43562638335fa960f10bd64ba3e6734
Hernan Di Tano <hditano@gmail.com>
2021-08-05 01:52:14 -0300

    added basic functionality of newCard()

--- script.js
+++ script.js
@@ -7,20 +7,29 @@ let isAlive = true;
 
 let message = "";
 
-let messageEl = document.querySelector('#message-el');
+let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardEl = document.querySelector('#card-el');
 
-function startGame () {
-    if(sum <= 20) {
-        message = 'Do you want to draw a new card?';
-    } else if (sum === 21) {
-        message = 'Woohoo!! Blackjack!!!!';
-        hasBlackJack = true;
-    } else {
-        message = 'You are out of the game!!';
-        isAlive = false;
-    }
+function startGame() {
+  cardEl.textContent = "Cards: " + firstCard + " " + secondCard;  
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Woohoo!! Blackjack!!!!";
+    hasBlackJack = true;
+  } else {
+    message = "You are out of the game!!";
+    isAlive = false;
+  }
 
-    messageEl.textContent = message;
-
+  messageEl.textContent = message;
 }
 
+function newCard() {
+    let card = 5;
+    sum += card;
+    startGame();
+    console.log('Drawing a new card from the deck');
+}
